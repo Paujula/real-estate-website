8e191cfc77a3fb1d159be9d9342d6ac6d08bc53d
Paujula <winnermariette@gmail.com>
2024-05-17 23:19:09 +0200

    Projet modifié le soir du 17/05/2024

---
+++ home.js
@@ -0,0 +1,16 @@
+<div class="input-container2">
+<div class="inner-input2" >
+    <input type="text" style="width: 300px; height: 100px;">
+    <input type="text">
+    <input type="text">
+    <input type="text">
+</div>
+</div>
+
+.input-container2 {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin-top: 10px;
+  
+}
